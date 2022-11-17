9d72f09ca3baf755002d5f8817071107a9fa7f97
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-11-16 22:11:08 -0500

    assignment complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
-def oxford_comma(array)
+require 'pry'
 
+# oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+# => "fiddleheads, okra, and kohlrabi"
+# Test array above. My attempt at solving below.
+# def oxford_comma(array)
+#   str = array.join("," + " ")
+#   if array.length > 3
+#     array[-2].insert("and" + " ")
+#   elsif 
+#     str 
+#   end
+# end
+
+def oxford_comma(array)
+  return array.join(" and ") if array.size < 3
+
+  # insert 'and ' in front of the last string in the array
+  array[-1] = "and #{array[-1]}"
+
+  array.join(", ")  
 end
